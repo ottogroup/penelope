95a89639d205f99db8be55ce16a5a1d7d23a842a
KaanTolunayKilic <kaan-tolunay@outlook.de>
2022-06-08 13:31:12 +0200

    adds GITHUB_STEP_SUMMARY to test

---
+++ .github/scripts/test.sh
@@ -0,0 +1,16 @@
+export POSTGRES_DB=backupdatabase
+export POSTGRES_USER=backupuser
+export POSTGRES_PASSWORD=backupuserpassword
+export POSTGRES_PORT=5432
+
+go test -v -p 1 ./... | tee tmp-test-output.txt
+
+passed=$(cat tmp-test-output.txt | grep -c "\-\-\- PASS:")
+failed=$(cat tmp-test-output.txt | grep -c "\-\-\- FAIL:")
+skipped=$(cat tmp-test-output.txt | grep -c "\-\-\- SKIP:")
+
+echo "" > $GITHUB_STEP_SUMMARY
+echo "| ✅ PASSED      | 🚫 FAILED    | ⏭ SKIPPED    |" >> $GITHUB_STEP_SUMMARY
+echo "| -----------: | ---------: | ---------:  |" >> $GITHUB_STEP_SUMMARY
+echo "| $passed     | $failed   | $skipped   |" >> $GITHUB_STEP_SUMMARY
+
