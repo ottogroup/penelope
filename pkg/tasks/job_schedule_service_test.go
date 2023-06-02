ebeb1724ba62c928d34931ac7ccdcb85a402c0d7
Boris M. Wagner <boris.wagner@ottogroup.com>
2023-06-02 13:12:42 +0200

    GBICSGO-345:test patch StorageTransferJob

--- pkg/tasks/job_schedule_service_test.go
+++ pkg/tasks/job_schedule_service_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"github.com/ottogroup/penelope/pkg/http/mock"
 	"github.com/ottogroup/penelope/pkg/processor"
 	"github.com/ottogroup/penelope/pkg/repository"
 	"github.com/ottogroup/penelope/pkg/secret"
@@ -12,6 +13,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"io"
+	"net/http"
 	"os"
 	"strings"
 	"testing"
@@ -21,6 +23,9 @@ import (
 const (
 	scheduleServiceJobID    = "schedule-uuid-1234"
 	scheduleServiceBackupID = "schedule-uuid-5678"
+	sourceProject           = "local-ability"
+	transferJobID1          = "transferJobs/1234567890123456789"
+	transferJobID2          = "transferJobs/0123456789012345678"
 )
 
 type MockScheduleProcessor struct {
@@ -306,6 +311,201 @@ func TestJobScheduleService_WithValidJobValidBigQueryBackup(t *testing.T) {
 	assert.Equalf(t, repository.Scheduled, updatedJob.Status, "Job with id %s should be be scheduled but has status %s", scheduleServiceJobID, updatedJob.Status)
 }
 
+func TestJobScheduleService_WithValidJobValidCloudStorageBackupAndReusableJob(t *testing.T) {
+	// ARRANGE
+
+	// Three calls to StorageTransfer API are expected. A job has been set to not started with a pre-configured transfer job id
+	// 1. Check if this transfer job still exists. A job will be returned, with a matching targetProjectID.
+	templateValues := map[string]string{
+		"TransferJobID":   transferJobID1,
+		"TargetProjectID": targetProjectID,
+	}
+	mockGetResponse, err := mock.SimpleResponseBodyFromTemplate(cloudStorageGetResponseTpl, templateValues, http.StatusOK)
+	require.NoError(t, err)
+	httpMockHandler.Register(mock.NewMockedHTTPRequest("GET", fmt.Sprintf("/v1/%s", transferJobID1), mockGetResponse))
+	// 2. Since it exists the existing job is updated with a PATCH operation.
+	mockPostResponse, err := mock.SimpleResponseBodyFromTemplate(cloudStorageGetResponseTpl, templateValues, http.StatusOK)
+	require.NoError(t, err)
+	httpMockHandler.Register(mock.NewMockedHTTPRequest("PATCH", "/v1/transferJobs.*", mockPostResponse))
+	// 3. A patched job without schedule (Penelope manages scheduling itself and never sets a schedule on a transferJob)
+	//    needs to be triggered separately.
+	mockPostTransferOperationResponse, err := mock.SimpleResponseBodyFromTemplate(cloudStorageRunResponseTpl, templateValues, http.StatusOK)
+	require.NoError(t, err)
+
+	httpMockHandler.Register(mock.NewMockedHTTPRequest("POST", "/v1/transferJobs/9099182517880104661:run", mockPostTransferOperationResponse))
+	httpMockHandler.Start()
+	defer httpMockHandler.Stop()
+
+	configProvider := &MockImpersonatedTokenConfigProvider{
+		TargetPrincipal: "backup-tooling@local-test-prod.iam.gserviceaccount.com",
+		Error:           nil,
+	}
+
+	ctx := context.Background()
+	backupRepository, err := repository.NewBackupRepository(ctx, secret.NewEnvSecretProvider())
+	require.NoErrorf(t, err, "BackupRepository should be instantiate")
+
+	jobRepository, err := repository.NewJobRepository(ctx, secret.NewEnvSecretProvider())
+	require.NoErrorf(t, err, "JobRepository should be instantiate")
+
+	scheduleService, err := newJobScheduleService(ctx, configProvider, secret.NewEnvSecretProvider())
+	require.NoErrorf(t, err, "JobScheduleService should be instantiate")
+
+	backup := repository.Backup{
+		ID:            scheduleServiceBackupID,
+		Status:        repository.NotStarted,
+		SourceProject: sourceProject,
+		Strategy:      repository.Snapshot,
+		Type:          repository.CloudStorage,
+		SinkOptions: repository.SinkOptions{
+			TargetProject: targetProjectID,
+			Sink:          "uuid-5678-123456",
+			Region:        "europe-west1",
+			StorageClass:  repository.Nearline.String(),
+		},
+		BackupOptions: repository.BackupOptions{
+			CloudStorageOptions: repository.CloudStorageOptions{Bucket: "test-bucket"},
+		},
+	}
+	_, err = backupRepository.AddBackup(ctx, &backup)
+	require.NoError(t, err, "should add new backup")
+	defer func() { deleteBackup(scheduleServiceBackupID) }()
+
+	job1 := repository.Job{
+		ID:           scheduleServiceJobID,
+		BackupID:     scheduleServiceBackupID,
+		Type:         repository.CloudStorage,
+		Status:       repository.NotScheduled,
+		Source:       "amount_budget_plan",
+		ForeignJobID: repository.ForeignJobID{CloudStorageID: transferJobID1},
+		EntityAudit:  repository.EntityAudit{CreatedTimestamp: time.Now().Add(-4 * time.Hour)},
+	}
+	job2 := repository.Job{
+		ID:           "schedule-uuid-0123",
+		BackupID:     scheduleServiceBackupID,
+		Type:         repository.CloudStorage,
+		Status:       repository.NotScheduled,
+		Source:       "amount_budget_plan",
+		ForeignJobID: repository.ForeignJobID{CloudStorageID: transferJobID2},
+		EntityAudit:  repository.EntityAudit{CreatedTimestamp: time.Now().Add(-48 * time.Hour)},
+	}
+	err = jobRepository.AddJob(ctx, &job1)
+	require.NoError(t, err, "should add new job")
+	err = jobRepository.AddJob(ctx, &job2)
+	require.NoError(t, err, "should add new job")
+	defer func() {
+		jobRepository.DeleteJob(ctx, scheduleServiceJobID)
+		jobRepository.DeleteJob(ctx, "schedule-uuid-0123")
+	}()
+
+	// ACT
+	scheduleService.Run(ctx)
+
+	// ASSERT
+	updatedJob, err := jobRepository.GetJob(ctx, scheduleServiceJobID)
+	require.NoError(t, err)
+	assert.Equalf(t, repository.Scheduled, updatedJob.Status, "Job with id %s should be be scheduled but has status %s", scheduleServiceJobID, updatedJob.Status)
+	assert.Equal(t, transferJobID1, updatedJob.CloudStorageID.String())
+}
+
+func TestJobScheduleService_WithValidJobValidCloudStorageBackupAndNotReusableJob(t *testing.T) {
+	// ARRANGE
+
+	// Two calls to StorageTransfer API are expected. A job has been set to NotStarted with a pre-configured transfer job id
+	// 1. Check if this transfer job still exists. A job will be returned, but with a targetProjectID not matching the desired
+	//    backup spec.
+	templateValues := map[string]string{
+		"TransferJobID":   transferJobID1,
+		"TargetProjectID": "another-project-id",
+	}
+	mockGetTransferJobResponse, err := mock.SimpleResponseBodyFromTemplate(cloudStorageGetResponseTpl, templateValues, http.StatusOK)
+	require.NoError(t, err)
+	httpMockHandler.Register(mock.NewMockedHTTPRequest("GET", fmt.Sprintf("/v1/%s", transferJobID1), mockGetTransferJobResponse))
+
+	// 2. Since the pre-configured transfer job id does not match the configuration a new transfer job is being created
+	//    and the POST request returns a new transfer job with a new transfer job id. We expect the new penelope job entry
+	//    in the db to contain this new entry as the transfer job id.
+	templateValues["TargetProjectID"] = targetProjectID
+	templateValues["TransferJobID"] = transferJobID2
+	mockPostTransferJobResponse, err := mock.SimpleResponseBodyFromTemplate(cloudStorageGetResponseTpl, templateValues, http.StatusOK)
+	require.NoError(t, err)
+	httpMockHandler.Register(mock.NewMockedHTTPRequest("POST", "/v1/transferJobs.*", mockPostTransferJobResponse))
+
+	httpMockHandler.Start()
+	defer httpMockHandler.Stop()
+
+	configProvider := &MockImpersonatedTokenConfigProvider{
+		TargetPrincipal: "backup-tooling@local-test-prod.iam.gserviceaccount.com",
+		Error:           nil,
+	}
+
+	ctx := context.Background()
+	backupRepository, err := repository.NewBackupRepository(ctx, secret.NewEnvSecretProvider())
+	require.NoErrorf(t, err, "BackupRepository should be instantiate")
+
+	jobRepository, err := repository.NewJobRepository(ctx, secret.NewEnvSecretProvider())
+	require.NoErrorf(t, err, "JobRepository should be instantiate")
+
+	scheduleService, err := newJobScheduleService(ctx, configProvider, secret.NewEnvSecretProvider())
+	require.NoErrorf(t, err, "JobScheduleService should be instantiate")
+
+	backup := repository.Backup{
+		ID:            scheduleServiceBackupID,
+		Status:        repository.NotStarted,
+		SourceProject: sourceProject,
+		Strategy:      repository.Snapshot,
+		Type:          repository.CloudStorage,
+		SinkOptions: repository.SinkOptions{
+			TargetProject: targetProjectID,
+			Sink:          "uuid-5678-123456",
+			Region:        "europe-west1",
+			StorageClass:  repository.Nearline.String(),
+		},
+		BackupOptions: repository.BackupOptions{
+			CloudStorageOptions: repository.CloudStorageOptions{Bucket: "test-bucket"},
+		},
+	}
+	_, err = backupRepository.AddBackup(ctx, &backup)
+	require.NoError(t, err, "should add new backup")
+	defer func() { deleteBackup(scheduleServiceBackupID) }()
+
+	job1 := repository.Job{
+		ID:           scheduleServiceJobID,
+		BackupID:     scheduleServiceBackupID,
+		Type:         repository.CloudStorage,
+		Status:       repository.NotScheduled,
+		Source:       "amount_budget_plan",
+		ForeignJobID: repository.ForeignJobID{CloudStorageID: transferJobID1},
+		EntityAudit:  repository.EntityAudit{CreatedTimestamp: time.Now().Add(-4 * time.Hour)},
+	}
+	job2 := repository.Job{
+		ID:           "schedule-uuid-0123",
+		BackupID:     scheduleServiceBackupID,
+		Type:         repository.CloudStorage,
+		Status:       repository.NotScheduled,
+		Source:       "amount_budget_plan",
+		ForeignJobID: repository.ForeignJobID{CloudStorageID: transferJobID2},
+		EntityAudit:  repository.EntityAudit{CreatedTimestamp: time.Now().Add(-48 * time.Hour)},
+	}
+	err = jobRepository.AddJob(ctx, &job1)
+	require.NoError(t, err, "should add new job")
+	err = jobRepository.AddJob(ctx, &job2)
+	require.NoError(t, err, "should add new job")
+	defer func() {
+		jobRepository.DeleteJob(ctx, scheduleServiceJobID)
+		jobRepository.DeleteJob(ctx, "schedule-uuid-0123")
+	}()
+
+	// ACT
+	scheduleService.Run(ctx)
+
+	// ASSERT
+	updatedJob, err := jobRepository.GetJob(ctx, scheduleServiceJobID)
+	require.NoError(t, err)
+	assert.Equalf(t, repository.Scheduled, updatedJob.Status, "Job with id %s should be be scheduled but has status %s", scheduleServiceJobID, updatedJob.Status)
+	assert.Equal(t, transferJobID2, updatedJob.CloudStorageID.String())
+}
+
 func TestJobScheduleService_WithValidJobValidCloudStorageBackup(t *testing.T) {
 	httpMockHandler.Start()
 	defer httpMockHandler.Stop()
@@ -437,3 +637,59 @@ func dropSourceMetadata(backupID string) error {
 		Delete()
 	return err
 }
+
+var cloudStorageGetResponseTpl = `{
+  "name": "{{ .TransferJobID }}",
+  "description": "Job to transfer oghub-acme-b-d:demo_deleteme to oghub-acme-backup:bkp_gcs_1290eef2-b3cf-4609-84e0-13ad4c523d1b. Triggered by BackupApp in project oghub-backup",
+  "projectId": "{{ .TargetProjectID }}",
+  "transferSpec": {
+    "gcsDataSource": {
+      "bucketName": "demo_deleteme"
+    },
+    "gcsDataSink": {
+      "bucketName": "bkp_gcs_1290eef2-b3cf-4609-84e0-13ad4c523d1b"
+    },
+    "objectConditions": {}
+  },
+  "schedule": {
+    "scheduleStartDate": {
+      "year": 2023,
+      "month": 6,
+      "day": 2
+    },
+    "scheduleEndDate": {
+      "year": 2023,
+      "month": 6,
+      "day": 2
+    }
+  },
+  "status": "ENABLED",
+  "creationTime": "2023-06-02T07:05:05.893622244Z",
+  "lastModificationTime": "2023-06-02T07:05:05.893622244Z",
+  "latestOperationName": "transferOperations/{{ replace .TransferJobID "/" "-" }}-18044211220405807161"
+}`
+
+var cloudStorageRunResponseTpl = `{
+  "name": "transferOperations/{{ replace .TransferJobID "/" "-" }}-6840339026955755396",
+  "metadata": {
+    "@type": "type.googleapis.com/google.storagetransfer.v1.TransferOperation",
+    "name": "transferOperations/{{ replace .TransferJobID "/" "-" }}-6840339026955755396",
+    "projectId": "{{ .TargetProjectID }}",
+    "transferSpec": {
+      "gcsDataSource": {
+        "bucketName": "demo_deleteme"
+      },
+      "gcsDataSink": {
+        "bucketName": "bkp_gcs_76efa173-e2cd-42db-be6a-c0a63ddcf215"
+      },
+      "objectConditions": {}
+    },
+    "startTime": "2023-06-02T10:28:56.541506055Z",
+    "status": "QUEUED",
+    "counters": {},
+    "transferJobName": "{{ .TransferJobID }}"
+  },
+  "response": {
+    "@type": "type.googleapis.com/google.protobuf.Empty"
+  }
+}`
