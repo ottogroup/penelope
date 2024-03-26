824a6c9e3df6161b089f7a47d5ea2dd95f978320
Lukas Janssen <lukas.janssen@ottogroup.com>
2024-03-26 17:02:24 +0100

    GBICSGO-336: finish backup details in vue

---
+++ frontend/src/models/api/models/RestoreResponse.ts
@@ -0,0 +1,13 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type RestoreResponse = {
+    backup_id?: string;
+    actions?: Array<{
+        action?: string;
+        type?: string;
+    }>;
+};
+
