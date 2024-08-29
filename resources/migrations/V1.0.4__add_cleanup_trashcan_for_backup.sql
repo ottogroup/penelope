86ee881184d39dd7e11bc7b0ff87fb41e606579d
KaanTolunayKilicOG <kaantolunay.kilic@ottogroup.com>
2024-08-29 12:47:58 +0200

    GBICSGO-2277: fix timestamp type for trashcan_cleanup_last_scheduled_timestamp

--- resources/migrations/V1.0.4__add_cleanup_trashcan_for_backup.sql
+++ resources/migrations/V1.0.4__add_cleanup_trashcan_for_backup.sql
@@ -3,4 +3,4 @@ ALTER TABLE backups
 ALTER TABLE backups
     ADD trashcan_cleanup_error_message TEXT DEFAULT NULL;
 ALTER TABLE backups
-    ADD trashcan_cleanup_last_scheduled_timestamp TEXT DEFAULT NULL;
+    ADD trashcan_cleanup_last_scheduled_timestamp TIMESTAMP DEFAULT NULL;
